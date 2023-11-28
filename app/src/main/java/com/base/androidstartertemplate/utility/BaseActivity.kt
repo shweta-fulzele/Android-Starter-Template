202bbcb0a39bc5948a9c514c82aab7862c84fcdd
Shweta-Fulzele <shweta.fulzele@thinkitive.com>
2023-11-28 17:25:42 +0530

    Added BaseActivity to Maintain network for overall application

---
+++ app/src/main/java/com/base/androidstartertemplate/utility/BaseActivity.kt
@@ -0,0 +1,52 @@
+package com.base.androidstartertemplate.utility
+
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.net.wifi.WifiManager
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import com.base.androidstartertemplate.utility.networkstate.NetworkStateReceiver
+open class BaseActivity: ComponentActivity() {
+
+    private var networkStateReceiver: NetworkStateReceiver? = null
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION)
+        intentFilter.addAction(WifiManager.WIFI_STATE_CHANGED_ACTION)
+        intentFilter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION)
+
+        if (networkStateReceiver == null) {
+            networkStateReceiver = NetworkStateReceiver()
+        }
+        registerReceiver(networkStateReceiver, intentFilter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(networkStateReceiver)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+}
---
+++ app/src/main/java/com/base/androidstartertemplate/utility/networkstate/NetworkStateReceiver.kt
@@ -0,0 +1,33 @@
+package com.base.androidstartertemplate.utility.networkstate
+
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import androidx.lifecycle.MutableLiveData
+
+class NetworkStateReceiver: BroadcastReceiver() {
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
+    override fun onReceive(context: Context, intent: Intent?) {
+//        TODO("Functions When the Network State has been changed")
+        context.let {
+            val connectivityManager = it.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val activeNetwork = connectivityManager.activeNetwork
+            val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
+            isUserConnected.value =
+                networkCapabilities?.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)?: false
+                        || networkCapabilities?.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)?: false
+        }
+    }
+
+    companion object {
+
+        private const val TAG = "NetworkStateReceiver"
+
+        @JvmStatic
+        var isUserConnected = MutableLiveData<Boolean>()
+            private set
+    }
+}
